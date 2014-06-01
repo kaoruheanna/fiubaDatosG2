0be9646d4827e378c9b4b557d8fbca6d9affa489
Shidoply <nicolasfernandezlema@gmail.com>
2014-06-01 14:18:20 -0300

    Fix: pequeño fix de un out of range en un string

--- LZCtx.cpp
+++ LZCtx.cpp
@@ -264,8 +264,8 @@ int LZCtx::descomprimir(string archivoEntrada, string archivoSalida){
 			paraAgregarContexto = this->tabla.getContextoActual();
 			paraAgregarCodigo = this->tabla.getLastCode();
 			paraAgregarString = nuevoString;
-			std::string::iterator it= --(nuevoString.end());
-			this->tabla.setContexto(*it);
+			//std::string::iterator it= --(nuevoString.end());
+			this->tabla.setContexto(nuevoString.at(nuevoString.length() - 1));
 			hayUnStringSinTerminar = true;
 		}
 	}
